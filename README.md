05e63ddb34ab47db8f4bd72064e4ecad1982abfc
Mustapha BADAOUI <badaouimu@gmail.com>
2024-01-14 23:20:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sharing-Website
+## Installation using Docker Images
+To install Sharing-Website using Docker images, execute the following command:
+
+
+```bash
+docker run -p 5000:5000 -it badaouimu/sharing-website
+```
+
+## Manual Installation of Aslanpp tools:  
+### Install python (exemple sur Ubuntu): 
+```
+
+apt update -y
+apt install python3 pip -y
+pip3 install flask 
+```
+### Using of Website
+```
+git clone https://github.com/BadaouiMu/Sharing-Website.git
+
+cd Sharing-Website/
+python3 app.py
+```
+Visit the website at http://YourIP:5000 or use localhost:5000
+## Usage : 
+Once on the website, you can upload new files or download files that have already been uploaded.
---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def home():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('home.html', files=files)
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('home'))
+
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
